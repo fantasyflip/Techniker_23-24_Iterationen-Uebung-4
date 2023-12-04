fc21df263bb4e9d62d07a036fcdd0e0301b5ed36
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2023-12-04 10:23:32 +0100

    feat(main): solution for task 1

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+void task1(){
+    printf("Aufgabe 1: Fakultät\n\n");
+
+    //Variablen definieren
+    int input = 9, output = 1;
+
+    //Schleife so oft wiederholen wie der input
+    for(int i = 1; i <= input; i ++){
+        //i mit dem output multiplizieren
+        output = i * output;
+    }
+
+    //Ergebnis ausgeben
+    printf("Die Fakultät der Zahl %i ist %i\n\n", input, output);
+}
+
 int main()
 {
-    printf("Hello World!\n");
+    task1();
+
     return 0;
 }
